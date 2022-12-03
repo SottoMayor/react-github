564fff8561120322496f109c02107ede56f0f384
David Sotto Mayor <ddsmmf@gmail.com>
2022-12-03 12:44:28 -0400

    Creating the Header layout component

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { BsGithub } from 'react-icons/bs'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className="py-4 px-[2%] flex items-center flex-col md:justify-between md:flex-row bg-secondary ">
+        <BsGithub color="white" className="mb-2 md:mb-0" size={38}/>
+        <h1 className="text-white text-2xl">React GitHub</h1>
+    </header>
   )
 }
 

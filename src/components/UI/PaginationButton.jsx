db7e4adc56f74b046d0168782df65841592d9b82
David Sotto Mayor <ddsmmf@gmail.com>
2022-12-03 13:32:58 -0400

    Fixing the PaginationBox component

--- src/components/UI/PaginationButton.jsx
+++ src/components/UI/PaginationButton.jsx
@@ -5,7 +5,7 @@ const PaginationButton = (props) => {
         <button
             type={props.type || 'button'}
             onclick={props.onclick}
-            className="bg-tertiary text-primary hover:text-white mx-2 p-2 rounded-full w-10 font-bold transition-all cursor-pointer duration-200 cursor-default"
+            className="bg-tertiary text-primary hover:text-white mx-2 p-2 rounded-full w-10 font-bold transition-all cursor-pointer duration-200"
         >
             {props.children}
         </button>

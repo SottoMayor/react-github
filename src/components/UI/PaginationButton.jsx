49da1ebccd9fef60515801121eebf7d6a36356e4
David Sotto Mayor <ddsmmf@gmail.com>
2022-12-03 13:26:24 -0400

    Creating the PaginationButton UI component

---
+++ src/components/UI/PaginationButton.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const PaginationButton = (props) => {
+    return (
+        <button
+            type={props.type || 'button'}
+            onclick={props.onclick}
+            className="bg-tertiary text-primary hover:text-white mx-2 p-2 rounded-full w-10 font-bold transition-all duration-200 cursor-default"
+        >
+            {props.children}
+        </button>
+    );
+};
+
+export default PaginationButton;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import UserCard from '../components/Home/UserCard';
+import PaginationButton from '../components/UI/PaginationButton';
+
+const USERS = [
+    { id: 1, login: 'SottoMayor' },
+    { id: 2, login: 'John Doe' },
+    { id: 3, login: 'Mambo' },
+    { id: 4, login: 'Chocolate' },
+    { id: 5, login: 'Funny User' },
+];
 
 const Home = () => {
     return (
         <>
-            <UserCard id='1' login='SottoMayor'/>
-            <UserCard id='2' login='John Doe'/>
+            {USERS.map((user) => (
+                <UserCard {...user} />
+            ))}
+            <section className="flex items-center justify-center">
+                <PaginationButton>1</PaginationButton>
+                <PaginationButton>2</PaginationButton>
+            </section>
         </>
     );
 };

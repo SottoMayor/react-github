4b0caf77287f956d143684fb131ff6b1845badd1
David Sotto Mayor <ddsmmf@gmail.com>
2022-12-04 14:49:45 -0400

    Adding some logic to show and hide the repos information

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,22 +1,46 @@
 import React from 'react';
 import { Route, Routes, useLoaderData } from 'react-router-dom';
-import UserDescriptionContainer from '../components/Details/UserDescriptionContainer';
 import UserDetails from '../components/Details/UserDetails';
 import Repos from '../pages/Repos';
 import { getUser } from '../util/api';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 const USERS = [
-    {identifier: 'ID', value: '66526913'},
-    {identifier: 'User', value: 'SottoMayor'},
-    {identifier: 'Created at', value: '2020-06-06'},
-]
+    { identifier: 'ID', value: '66526913' },
+    { identifier: 'User', value: 'SottoMayor' },
+    { identifier: 'Created at', value: '2020-06-06' },
+];
 
 const Details = () => {
+    const [showRepos, setShowRepos] = useState(false);
+
     const userData = useLoaderData();
+
+    const showReposHandler = () => {
+        setShowRepos(prevState => !prevState)
+    }
+
+    let showReposContent;
+    if (showRepos) {
+        showReposContent = (
+            <Link onClick={showReposHandler} to={``}>Ocultar repositorios de SottoMayor</Link>
+        );
+    }else{
+        showReposContent = (
+            <Link onClick={showReposHandler} to={`repos`}>Ver repositórios de SottoMayor</Link>
+        )
+    }
 
     return (
         <>
-            <UserDetails avatar='https://avatars.githubusercontent.com/u/66526913?v=4' userInfoArray={USERS}/>
+            <UserDetails
+                avatar="https://avatars.githubusercontent.com/u/66526913?v=4"
+                userInfoArray={USERS}
+            />
+            
+            {showReposContent}
+
             <Routes>
                 <Route path="/repos" element={<Repos />} />
             </Routes>

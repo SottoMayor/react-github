f5136c289c64e2637d6fcd70987126648a698886
David Sotto Mayor <ddsmmf@gmail.com>
2022-12-04 14:21:13 -0400

    Styling the Details page

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,8 @@ const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path="/" element={<Main />}>
             <Route index element={<Home />} loader={homeLoader} />
-            <Route path="/details/:id/*" element={<Details />} loader={detailsLoader} />
+            {/* <Route path="/details/:id/*" element={<Details />} loader={detailsLoader} /> */}
+            <Route path="/details/:id/*" element={<Details />} />
             <Route path="*" element={<NotFound />} />
         </Route>
     )
--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -4,13 +4,34 @@ import Repos from '../pages/Repos';
 import { getUser } from '../util/api';
 
 const Details = () => {
-
-    const userData = useLoaderData()
-    
+    const userData = useLoaderData();
 
     return (
         <>
-            <div>Details</div>
+            <section>
+                <figure className="mx-auto max-w-[300px] border-tertiary border-4 rounded-full mb-10 md:mb-24">
+                    <img
+                        className="w-full rounded-full p-3"
+                        src="https://avatars.githubusercontent.com/u/66526913?v=4"
+                        alt="avatar"
+                    />
+                </figure>
+
+                <div className="flex justify-evenly items-center flex-col md:flex-row">
+                    <h1 className="text-2xl">
+                        ID: <strong>66526913</strong>
+                    </h1>
+                    <h1 className="text-2xl">
+                        User: <strong>SottoMayor</strong>
+                    </h1>
+                    <h1 className="text-2xl">
+                        Created at: <strong>2020-06-06</strong>
+                    </h1>
+                </div>
+
+                <div className="bg-tertiary py=1 w-full h-1 rounded"></div>
+            </section>
+
             <Routes>
                 <Route path="/repos" element={<Repos />} />
             </Routes>
@@ -18,10 +39,10 @@ const Details = () => {
     );
 };
 
-export const detailsLoader = async ({params}) => {
-   const { id: userId } = params;
+export const detailsLoader = async ({ params }) => {
+    const { id: userId } = params;
 
-    return getUser(userId)
-}
+    return getUser(userId);
+};
 
 export default Details;

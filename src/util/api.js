1fab69b69c941e4379b37f6df7968b253fe35007
David Sotto Mayor <ddsmmf@gmail.com>
2022-12-04 13:51:15 -0400

    Creating the api call to get users details

--- src/util/api.js
+++ src/util/api.js
@@ -1,9 +1,19 @@
 import axios from 'axios';
 
 export const getUsers = async (since) => {
-    console.log(import.meta.env.VITE_APP_API_URL)
     try{
         const response = await axios.get(`${import.meta.env.VITE_APP_API_URL}/api/users?since=${since || 0}`)
+
+        return response.data;
+    }catch(e){
+        // In this test I'm not worried about handling errors.
+        console.log(e)
+    }
+}
+
+export const getUser = async (username) => {
+    try{
+        const response = await axios.get(`${import.meta.env.VITE_APP_API_URL}/api/users/${username}/details`)
 
         return response.data;
     }catch(e){
